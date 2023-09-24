4506d3935776883db556e8f1bd2a378ac39e6c33
umarfarooq361 <umarrje36@gmail.com>
2023-09-24 22:56:39 +0500

    eth_getStorageAt() on Swisstronik, it returns encrypted values because the contract state is

--- scripts/getStorage.js
+++ scripts/getStorage.js
@@ -14,6 +14,19 @@ async function main() {
   const storageValue = await provider.send("eth_getStorageAt", [contractAddress, slot, "latest"]);
 
   console.log("Storage Value:", storageValue);
+  /*
+In swisstronic`eth_getStorageAt()` returns an encrypted value, is in line with the Swisstronik blockchain's 
+design for enhanced privacy and confidentiality. 
+Swisstronik implements the Ethereum Virtual Machine (EVM) within Intel SGX to enhance the privacy of 
+transaction data and smart contracts. As a result, the contract state variables are only visible and 
+accessible within the contract itself, and even full nodes and validators cannot access raw contract data.
+
+When you use `eth_getStorageAt()` on Swisstronik, it returns encrypted values because the contract state is 
+encrypted to ensure confidentiality. This is a significant departure from the behavior you might be familiar 
+with on the Ethereum mainnet or other Ethereum-compatible blockchains, where you can directly access the 
+values of storage slots.
+
+   */
 }
 
 main().catch((error) => {
